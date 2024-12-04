f70ffe38dfac63502715732a0e654741569957cd
Prince Mishra <pm618209@gmail.com>
2024-12-04 21:54:17 +0530

    Redux toolkit setup

--- backend/utils/dbConnection.js
+++ backend/utils/dbConnection.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config({});
 
 // const connectDB = async () => {
 //   try {
@@ -8,15 +10,18 @@ import mongoose from "mongoose";
 //     console.log("Database  Connection Failed", error);
 //   }
 // };
+if (!process.env.DATABASE_URL) {
+  console.error("DATABASE_URL is not defined in the environment variables.");
+  process.exit(1);
+}
 
-const connectDB = async() => {
+const connectDB = async () => {
   try {
-      await mongoose.connect("mongodb://localhost:27017/instagramClone_yt")
-      console.log("DAta base connected");
-      
+    await mongoose.connect("mongodb://localhost:27017/instagramClone_yt");
+    console.log("DAta base connected");
   } catch (error) {
     console.log("Database  Connection Failed", error);
   }
-}
+};
 
-export  default connectDB;
+export default connectDB;
